ddfd213f233791b102f03f58195548cd1b615fc4
Abozer <a.abozer.n@gmail.com>
2026-08-26 17:36:42 +0000

    modofied

--- src/main.c
+++ src/main.c
@@ -1,30 +1,70 @@
 #include "header.h"
 
 
+int	g_signal_status = 0;
 
 
-
-
-int	main(int argc, char **argv)
+static void	shell_loop(char **env)
 {
-	char	*line;
-	(void)argc;
-	(void)argv;
+	char		*line;
+	t_token		*tokens;
+	t_command	*commands;
+	int			status;
+
+	status = 0;
+
 	while (1)
 	{
 		line = readline("minishell$ ");
-		if (line == NULL)
+
+		if (!line)
+		{
+			printf("exit\n");
 			break ;
+		}
+
 		if (line[0] != '\0')
 			add_history(line);
+
 		tokens = lexer(line);
-		if (tokens != NULL)
+
+		if (tokens)
 		{
 			commands = parser(tokens);
-			execute(commands);
+
+			if (commands)
+			{
+				expansion(commands, env, status);
+
+				executor(commands, env);
+			}
+
+			free_commands(commands);
+			token_clear(&tokens);
 		}
-		free_all_for_this_line(...);
+
 		free(line);
 	}
+}
+
+
+int	main(int argc, char **argv, char **env)
+{
+	char	**shell_env;
+
+	(void)argc;
+	(void)argv;
+
+	setup_signals();
+
+	shell_env = copy_env(env);
+
+	if (!shell_env)
+		return (1);
+
+	shell_loop(shell_env);
+
+	free_env(shell_env);
+
 	return (0);
 }
